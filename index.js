42da1c917152b73991d621629eb41a845d9ed943
Joachim Van Herwegen <joachimvh@gmail.com>
2017-10-31 17:02:38 +0100

    Remove ignored .js files

--- index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const translate = require("./lib/sparqlAlgebra");
-const Algebra = require("./lib/algebra");
-module.exports = {
-    translate,
-    Algebra
-};
-//# sourceMappingURL=index.js.map
