3f567804aa879aac591ac5455b97c6ddde4d629f
Joachim Van Herwegen <joachimvh@gmail.com>
2018-02-08 10:27:16 +0100

    Allow generic test folder structure

--- test/parser-test.ts
+++ test/parser-test.ts
@@ -5,38 +5,36 @@ import * as path from 'path';
 import {translate} from '../index';
 import Util from './util';
 
+const rootSparql = 'test/sparql';
+const rootJson = 'test/algebra';
+
 // https://www.w3.org/2001/sw/DataAccess/tests/r2#syntax-basic-01
 // https://www.w3.org/2009/sparql/implementations/
 // https://www.w3.org/2009/sparql/docs/tests/
 describe('SPARQL Parser', () =>
 {
-    let rootSparql = 'test2/sparql';
-    let rootJson = 'test2/algebra';
     // dawg/sparql
     let subfolders = fs.readdirSync(rootSparql);
 
-    // TODO: support different folder structures
     for (let subfolder of subfolders)
-    {
-        // aggregates, bind, etc.
-        let types = fs.readdirSync(path.join(rootSparql, subfolder));
-        for (let type of types)
-        {
-            describe(subfolder + '/' + type, () =>
-            {
-                let queries = fs.readdirSync(path.join(rootSparql, subfolder, type));
-                for (let queryName of queries)
-                {
-                    it(queryName, () => {
-                        let query = fs.readFileSync(path.join(rootSparql, subfolder, type, queryName), 'utf8');
-                        let name = queryName.replace(/\.sparql$/, '');
-                        let jsonName = name + '.json';
-                        let expected = JSON.parse(fs.readFileSync(path.join(rootJson, subfolder, type, jsonName), 'utf8'));
-                        let algebra = Util.objectify(translate(query, { quads: name.endsWith('(quads)') }));
-                        expect(algebra).to.deep.equal(expected);
-                    })
-                }
-            });
-        }
-    }
+        testPath(subfolder, subfolder);
 });
+
+function testPath(fileName: string, testName: string): void
+{
+    let sparqlName = path.join(rootSparql, fileName);
+    if (fs.lstatSync(sparqlName).isDirectory())
+    {
+        for (let sub of fs.readdirSync(sparqlName))
+            testPath(path.join(fileName, sub), testName + '/' + sub);
+    } else {
+        let name = testName.replace(/\.sparql$/, '');
+        it (name, () =>
+        {
+            let query = fs.readFileSync(sparqlName, 'utf8');
+            let expected = JSON.parse(fs.readFileSync(path.join(rootJson, fileName.replace(/\.sparql$/, '.json')), 'utf8'));
+            let algebra = Util.objectify(translate(query, { quads: name.endsWith('(quads)') }));
+            expect(algebra).to.deep.equal(expected);
+        });
+    }
+}
