782e48162392ad6139062613324d8f485ba9e18e
Joachim Van Herwegen <joachimvh@gmail.com>
2019-04-19 14:11:04 +0200

    Add CLI support for algebra -> SPARQL

--- bin/sparqlalgebrajs.ts
+++ bin/sparqlalgebrajs.ts
@@ -2,10 +2,12 @@
 
 import * as minimist from 'minimist';
 import translate from '../lib/sparqlAlgebra';
+import {toSparql, toSparqlJs} from '../lib/sparql';
+import Util from "../test/util";
 
 const args = minimist(process.argv.slice(2), {
-    boolean: ['q'],
-    alias: { q: 'quads' }
+    boolean: ['q', 'r'],
+    alias: { q: 'quads', r: 'reverse' }
 });
 
 if (args.h || args.help || args._.length !== 1)
@@ -14,7 +16,15 @@ if (args.h || args.help || args._.length !== 1)
     console.error('options:');
     console.error('  -h --help  : Show this output.');
     console.error('  -q --quads : Apply GRAPH statements to convert triples to quads.');
+    console.error('  -r --reverse  : Convert algebra to SPARQL.');
     process.exit((args.h || args.help) ? 0 : 1);
 }
 
-console.log(JSON.stringify(translate(args._[0], { quads: args.q}), null, 2));
+if (args.r || args.reverse)
+{
+    console.log(toSparql(JSON.parse(args._[0])));
+}
+else
+{
+    console.log(JSON.stringify(Util.objectify(translate(args._[0], { quads: args.q })), null, 2));
+}
