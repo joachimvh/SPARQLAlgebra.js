0823d36cd12b30147185566b7a144e8ff90af4fc
Ruben Taelman <ruben.taelman@ugent.be>
2020-08-27 17:17:58 +0200

    Add --strict flag to disable SPARQL* mode

--- bin/sparqlalgebrajs.ts
+++ bin/sparqlalgebrajs.ts
@@ -6,17 +6,18 @@ import {toSparql} from '../lib/sparql';
 import Util from "../test/util";
 
 const args = minimist(process.argv.slice(2), {
-    boolean: ['q', 'r'],
-    alias: { q: 'quads', r: 'reverse' }
+    boolean: ['q', 'r', 's'],
+    alias: { q: 'quads', r: 'reverse', s: 'strict' }
 });
 
 if (args.h || args.help || args._.length !== 1)
 {
     console.error('usage: ./sparqlalgebrajs [-q/--quads] "SELECT * WHERE { ?x ?y ?z}"');
     console.error('options:');
-    console.error('  -h --help  : Show this output.');
-    console.error('  -q --quads : Apply GRAPH statements to convert triples to quads.');
-    console.error('  -r --reverse  : Convert algebra to SPARQL.');
+    console.error('  -h --help    : Show this output.');
+    console.error('  -q --quads   : Apply GRAPH statements to convert triples to quads.');
+    console.error('  -r --reverse : Convert algebra to SPARQL.');
+    console.error('  -s --strict  : Only allow parsing of pure SPARQL.');
     process.exit((args.h || args.help) ? 0 : 1);
 }
 
@@ -26,5 +27,5 @@ if (args.r || args.reverse)
 }
 else
 {
-    console.log(JSON.stringify(Util.objectify(translate(args._[0], { quads: args.q, sparqlStar: true })), null, 2));
+    console.log(JSON.stringify(Util.objectify(translate(args._[0], { quads: args.q, sparqlStar: !args.s })), null, 2));
 }
